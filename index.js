ed4d35654356a97f4de7cfd3320a35e1e4b9dcab
Hrant Aghayan <aghayanh66@gmail.com>
2022-03-05 12:25:16 +0400

    commit message

--- index.js
+++ index.js
@@ -1,116 +1,77 @@
-// 1
-// let a = +prompt("enter a number");
-// let b = +prompt("enter a number") ;
-// if(a > b)  {let sum = a + b;
-// let diference = a - b;
-// let product = a * b ;
-// let quotient = a / b ;
-// let remainder = a % b ;
-// console.log(sum,  diference,  product,  quotient,  remainder);
-// }
-// else{
-//     console.log("you have to take the first number greater than the second")
-// }
+
+// homework 2 
+1
+let number = +prompt("enter a number")
+let sum = 0
+while(number !== 0){
+    let lastdigit = number % 10
+    sum = sum + lastdigit
+    number = (number - lastdigit) / 10
+}
+console.log(sum)
 
 // 2
-// let name = "Hrant"
-// let age = 20
-// alert("My name is" + " " + name +" " + "i am" + " " + age)
-
-// let information = '"My name is Hrant, i am 20."'
-// alert(information)
+let number1 = +prompt("enter a number")
+let number2 = +prompt("enter a number")
+let number3 = +prompt("enter a number")
+if(number1 < number2 + number3 && number2 < number1 + number3 && number3< number1 + number2){
+    alert("yes")
+}else{
+    alert("no")
+// }
 
 // 3
-// let number = +prompt("Enter a number")
-// number = number % 10
-// alert(number) 
+let number = +prompt("enter a number")
+let b = 0
+if(number === 0){
+console.log(1)
+}else{while(number !== 0){
+let lastdigit = number % 10
+b++
+number = (number - lastdigit) / 10
+}
+console.log(b)
+}
 
 // 4
-// let number = +prompt("Enter a number")
-// if(number < 0 ){
-//     alert("yes")
-// }else{
-//     alert("no")
-// }
+let number = +prompt("enter a number")
+let b = 0
+while(number !== 0){
+let lastdigit = number % 10 
+if(lastdigit === 9 || lastdigit === -9){
+b++
+}
+number = (number - lastdigit) / 10
+}console.log(b)
 
 // 5
-// let a = +prompt("Enter a number")
-// let b = +prompt("Enter a number")
-// if(a % b === 0 || b % a === 0){
-//     alert(1)
-// }else{
-//     alert(0)
-// }
+let number = +prompt("enter a number")
+let b = 0
+while(number !== 0){
+let lastdigit = number % 10
+b = b * 10 + lastdigit
+number = (number - lastdigit) / 10
+}console.log(b)
 
-// 6
-// let month = +prompt("enter code which belong to [1,12] range")
-// switch(month){
-//  case(1): {
-//  alert("January")
-//  break;
-//  }
-//  case(2):{
-//  alert("February")
-//  break;    
-//  }
-//  case(3):{
-//  alert("march")
-//  break;    
-//  }
-//  case(4):{
-//  alert("April")
-//  break;    
-//  }
-//  case(5):{
-//  alert("May")
-//  break;    
-//  }
-//  case(6):{
-//  alert("June")
-//  break;
-//  }
-//  case(7):{
-//  alert("july")
-//  break;    
-//  }
-//  case(8):{
-//  alert("August")
-//  break;
-//  }
-//  case(9):{
-//  alert("September")
-//  break;     
-//  }
-//  case(10):{
-//  alert("October")
-//  break;    
-//  }
-//  case(11):{
-//  alert("November")
-//  break;    
-//  }
-//  case(12):{
-//  alert("December")
-//  break;    
-//  }
-//  default:{
-//  alert("the months are 12")    
-//  }
-// }
 
-// 7
-// let number1 = +prompt("enter a number")
-// let number2 = +prompt("enter a number")
-// let number3 = +prompt("enter a number")
-// if(number1 >= number2 && number1 >= number3){
-//     alert(number1 + " " + "is maximum")
-// }
-// else if(number2 >= number1 && number2 >= number3){
-//     alert(number2 + " " + "is maximum") 
-// }
-// else if(number3 >= number1 && number3 >= number1){
-//     alert(number3 + " " + "is maximum")
-// }
-// else{
-//     alert("there are no maximum number")
-// }
+
+// 2 tarberak stringov tarberakna
+let a = +prompt("enter a number")
+let b = " "
+let x = "-"
+if(a > 0) {while(a !== 0){
+let c = a % 10
+b = b + c 
+a = (a - c ) / 10
+}
+console.log(b)
+}else if(a < 0 )  {
+ while(a !== 0){
+let c = a % 10
+x = x + (-c) 
+a = (a - c ) / 10
+} 
+console.log(x)
+}else{
+    console.log(0)
+}
